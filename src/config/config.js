d10774a51d29b874946f61b88931ec5c363470e8
Matias R. Malamud <matiasrubiolo9546@gmail.com>
2025-01-23 18:14:31 -0300

    cambio para subida

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,12 @@ dotenv.config();
 import pkg from 'pg';
 const { Pool } = pkg;
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const configPool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false 
 });
 
 export const PORT = process.env.PORT || 3000;
-
 export const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY;
-
